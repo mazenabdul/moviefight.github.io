d5fd824c3cb06677575e2ca99892ab355dabd2a6
Mazen Abdul <63692059+mazenabdul@users.noreply.github.com>
2020-07-29 02:57:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: '3c8a4e3d',
                 s: searchTerm
